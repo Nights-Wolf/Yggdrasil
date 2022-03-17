4e2e5f7a4028c5a50f2daf0d197e3ec4db13044f
Nights_Wolf <dawinavi@gmail.com>
2022-03-17 21:05:34 +0100

    Hamburger, menu side switch, primary and secondary colors added

--- src/Header.js
+++ src/Header.js
@@ -3,18 +3,20 @@ import React from "react"
 function Header() {
     return(
         <header>
-            <h1>LOGO JAK BĘDZIE</h1>
+             <div className="hamburger--container">
+                <span className="hamburger"></span>
+                <span className="hamburger"></span>
+                <span className="hamburger"></span>
+            </div>
+            <h1>YGGDRASIL</h1>
             <form className="search-engine">
                 <input type="text"/>
             </form>
             <div className="login-panel">
                 <a href="/">Zaloguj się!</a>
                 <a href="/">Zarejestruj się!</a>
-            </div>
-            <div className="hamburger--container">
-                <span className="hamburger"></span>
-                <span className="hamburger"></span>
-                <span className="hamburger"></span>
+                <a href="/">O nas</a>
+                <a href="/">Kontakt</a>
             </div>
         </header>
 )}
