6081bfee30ea8a1cc1c6e70f3602b9c3d1a74b98
Nights_Wolf <dawinavi@gmail.com>
2022-04-12 23:21:45 +0200

    Header set success

--- src/react/Header.js
+++ src/react/Header.js
@@ -3,14 +3,12 @@ import React from "react"
 function Header() {
     return(
         <header>
-             <div className="hamburger--container">
-                <span className="hamburger"></span>
-                <span className="hamburger"></span>
-                <span className="hamburger"></span>
+            <div className="menu-btn">
+            <span className="menu-btn__hamburger"></span>
             </div>
             <form className="search-engine">
+                <button><i class="fa fa-eye"></i></button>
                 <input type="text"/>
-                <button></button>
             </form>
         </header>
 )}
