eaea3bc15760c6cde616440f23472772dfded246
Nights_Wolf <dawinavi@gmail.com>
2022-04-24 15:01:16 +0200

    Footer added

---
+++ src/react/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer>
+            <a href="/">Regulamin</a>
+            <a href="/">RODO</a>
+            <a href="/">Kontakt</a>
+            <p>Copyright &copy;</p>
+        </footer>
+    )
+}
+
+export default Footer
