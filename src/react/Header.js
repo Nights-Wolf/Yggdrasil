9a82aae135c5496490accef4bf9c4d33494c961f
Nights_Wolf <dawinavi@gmail.com>
2022-04-25 19:30:25 +0200

    nav bar added

--- src/react/Header.js
+++ src/react/Header.js
@@ -1,17 +1,38 @@
 import React from "react"
 
 function Header() {
-    return(
-        <header>
-            <div className="menu-btn">
-            <span className="menu-btn__hamburger"></span>
-            </div>
-            <form className="search-engine">
-                <button><i class="fa fa-eye"></i></button>
-                <input type="text"/>
-            </form>
-        </header>
+    const [mobileNavVisibility, setVisibility] = React.useState(false)
+
+        function showNavbar() {
+            setVisibility(prevShow => !prevShow)
+        }
+
+        const navVisibility = {
+            display: mobileNavVisibility ? 'flex' : 'none'
+        }
+
+        return(
+            <header>
+                <div className="menu-btn">
+                <span className="menu-btn__hamburger" onClick={showNavbar}></span>
+                </div>
+                <nav style={navVisibility}>
+                   <a href="/">Bursztyn</a>
+                    <a href="/">Topaz</a>
+                    <a href="/">Ametyst</a>
+                    <a href="/">Lapiz Lazuli</a>
+                    <a href="/">Kwarc</a>
+                    <a href="/">Kryształ Górski</a>
+                    <div className="nav-btn__login"><a href="/">Zaloguj się</a></div>
+                    <div className="nav-btn__register"><a href="/">Zarejestruj się</a></div>
+                </nav>
+                <form className="search-engine">
+                    <button><i class="fa fa-eye"></i></button>
+                    <input type="text"/>
+                </form>
+            </header>
 )}
+
 
 
 export default Header
--- src/react/Navbar.js
+++
@@ -1,8 +0,0 @@
-import Reac from "react"
-
-function Navbar() {
-    return(
-        <nav>
-        </nav>
-    )
-}
