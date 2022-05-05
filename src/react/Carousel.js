278fa6bf481c62f2980b0b7e0d68c740f0d57e0e
Nights_Wolf <dawinavi@gmail.com>
2022-05-05 23:57:03 +0200

    carousel arrow buttons added

--- src/react/Carousel.js
+++ src/react/Carousel.js
@@ -4,7 +4,7 @@ import discount_picture_2 from "../assets/images/promotion_image_2.jpg";
 
 function Carousel(props) {
     const [carouselElement, setCarouselElement] = React.useState([discount_picture_1, discount_picture_2])
-    const [displayCarouselElement, setCarouselElementToDisplay] = React.useState()
+    const [displayCarouselElement, setCarouselElementToDisplay] = React.useState(0)
 
     const visibleCarouselElementStyle = {
         transform: 'translate(0, 0)'
@@ -13,20 +13,29 @@ function Carousel(props) {
         transform: 'translate(-999px, 0)'
     }
 
-    function displayElements() {
-        let elementsToDisplay = []
+    function createElements() {
+        let elementsToCreate = []
         for(let i = 0; i < carouselElement.length; i++) {
-            elementsToDisplay.push(<img src={carouselElement[i]} style={i === 0 ? visibleCarouselElementStyle : hiddenCarouselElementStyle} alt="discount picture"></img>)
+            elementsToCreate.push(<img src={carouselElement[i]} style={i === 0 ? visibleCarouselElementStyle : hiddenCarouselElementStyle} alt="discount picture" aria-hidden={false}></img>)
         }
-        return(elementsToDisplay)
+        return elementsToCreate
+    }
+
+    function displayButtons() {
+        let buttonsToDisplay = []
+        for(let i = 0; i < carouselElement.length; i++) {
+            buttonsToDisplay.push(<button className="switch_disc_img" name={i}></button>)
+        }
+        return buttonsToDisplay
     }
 
     return(
         <>
-            {displayElements()}
+            {createElements()}
             <div className="button__group">
-                <button className="switch_disc_img"></button>
-                <button className="switch_disc_img"></button>
+                <button id="prev">&#10094;</button>
+                {displayButtons()}
+                <button id="next">&#10095;</button>
             </div>
         </>
     )
