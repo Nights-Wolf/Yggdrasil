3da80f97c72300752a13a7eff59d5776e680c5ad
Nights_Wolf <dawinavi@gmail.com>
2022-05-05 22:38:02 +0200

    Carousel elemenst styling based on array index

--- src/react/Carousel.js
+++ src/react/Carousel.js
@@ -6,10 +6,17 @@ function Carousel(props) {
     const [carouselElement, setCarouselElement] = React.useState([discount_picture_1, discount_picture_2])
     const [displayCarouselElement, setCarouselElementToDisplay] = React.useState()
 
+    const visibleCarouselElementStyle = {
+        transform: 'translate(0, 0)'
+    }
+    const hiddenCarouselElementStyle = {
+        transform: 'translate(-999px, 0)'
+    }
+
     function displayElements() {
         let elementsToDisplay = []
         for(let i = 0; i < carouselElement.length; i++) {
-            elementsToDisplay.push(<img src={carouselElement[i]} alt="discount picture"></img>)
+            elementsToDisplay.push(<img src={carouselElement[i]} style={i === 0 ? visibleCarouselElementStyle : hiddenCarouselElementStyle} alt="discount picture"></img>)
         }
         return(elementsToDisplay)
     }
