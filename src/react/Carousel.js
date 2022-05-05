48c59a5a6a55545a4b322259713679e227688f46
Nights_Wolf <dawinavi@gmail.com>
2022-05-05 22:22:31 +0200

    Dynamic rendering images in carousel

--- src/react/Carousel.js
+++ src/react/Carousel.js
@@ -3,12 +3,20 @@ import discount_picture_1 from "../assets/images/promotion_image_1.jpg";
 import discount_picture_2 from "../assets/images/promotion_image_2.jpg";
 
 function Carousel(props) {
-    const [carouselElement, setCarouselElement] = React.useState(false)
+    const [carouselElement, setCarouselElement] = React.useState([discount_picture_1, discount_picture_2])
+    const [displayCarouselElement, setCarouselElementToDisplay] = React.useState()
+
+    function displayElements() {
+        let elementsToDisplay = []
+        for(let i = 0; i < carouselElement.length; i++) {
+            elementsToDisplay.push(<img src={carouselElement[i]} alt="discount picture"></img>)
+        }
+        return(elementsToDisplay)
+    }
 
     return(
         <>
-            <img src={discount_picture_1} alt="discount picture"></img>
-            <img src={discount_picture_2} alt="discount picture"></img>
+            {displayElements()}
             <div className="button__group">
                 <button className="switch_disc_img"></button>
                 <button className="switch_disc_img"></button>
