00ec8043bb7a45cd480eb91adcbcb62f3a9221a2
Nights-Wolf <dawinavi@gmail.com>
2022-07-09 00:37:23 +0200

    Product page style added.

--- src/main/js/react/ProductPage.js
+++ src/main/js/react/ProductPage.js
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
+import discount_picture_2 from "./assets/images/promotion_image_2.jpg";
 
 function ProductPage(props) {
     return(
@@ -8,15 +9,15 @@ function ProductPage(props) {
             <Header />
             <section className="product--page">
                 <div className="product--details">
-                    <img/>
+                    <img src= {discount_picture_2} />
                     <div className="status">
-                        <p></p>
-                        <p></p>
-                        <Link to="/products"></Link>
+                        <p className="status--title">Bursztynowe drzewko najpięmkniejsze</p>
+                        <p className="status--availability">Dostępność: dostępny</p>
+                        <Link to="/products">DODAJ DO KOSZYKA</Link>
                     </div>
                 </div>
-                <div>
-                    <p></p>
+                <div className="description">
+                    <p>OPIS PRODUKTU</p>
                 </div>
             </section>
             <Footer />
