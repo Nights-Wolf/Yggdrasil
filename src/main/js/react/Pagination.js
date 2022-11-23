9697230258d9d8ebb2f7cc8857c8fe22e47db6a0
Nights-Wolf <dawinavi@gmail.com>
2022-11-23 23:01:06 +0100

    Pagination set (style WIP)

--- src/main/js/react/Pagination.js
+++ src/main/js/react/Pagination.js
@@ -1,7 +1,23 @@
-function Pagination() {
+import React from "react";
+
+const Pagination = ({productsPerPage, totalProducts, paginate}) => {
+    const url = window.location.href
+
+    const pageNumbers = []
+
+    for(let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
+        pageNumbers.push(i)
+    }
+
     return(
         <div className="pagination">
-        <span>1</span>
+            <ul>
+                {pageNumbers.map(number => (
+                    <li onClick={() => paginate(number)} key={number}>
+                        {number}
+                    </li>
+                ))}
+            </ul>
        </div>
     )
 }
